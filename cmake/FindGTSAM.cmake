0340f5cda7da46d16a1232ace72d78c52bf49a25
Ola Chr. Vaage <ola.christoffer.vage@scoutdi.com>
2026-03-20 14:32:43 +0100

    Refactor GTSAM find logic to use config-mode package for version handling

--- cmake/FindGTSAM.cmake
+++ cmake/FindGTSAM.cmake
@@ -1,33 +1,31 @@
 include_guard()
 
-find_path(GTSAM_INCLUDE_DIRS gtsam/inference/FactorGraph.h
-  HINTS /usr/local/include /usr/include
-  DOC "GTSAM include directories")
-
-find_library(GTSAM_LIB NAMES gtsam
-  HINTS /usr/local/lib /usr/lib
-  DOC "GTSAM libraries")
-
-find_library(GTSAM_UNSTABLE_LIB NAMES gtsam_unstable
-  HINTS /usr/local/lib /usr/lib
-  DOC "GTSAM_UNSTABLE libraries")
-
-find_library(TBB_LIB NAMES tbb
-  HINTS /usr/local/lib /usr/lib
-  DOC "TBB libraries")
-
-find_library(TBB_MALLOC_LIB NAMES tbbmalloc
-  HINTS /usr/local/lib /usr/lib
-  DOC "TBB malloc libraries")
-
-if(GTSAM_LIB AND GTSAM_UNSTABLE_LIB AND TBB_LIB)
-  set(GTSAM_LIBRARIES ${GTSAM_LIB} ${GTSAM_UNSTABLE_LIB} ${TBB_LIB} ${TBB_MALLOC_LIB})
+# Forward version and EXACT arguments to GTSAM's own config-mode package,
+# which handles version checking via GTSAMConfigVersion.cmake.
+set(_gtsam_find_args)
+if(GTSAM_FIND_VERSION)
+  list(APPEND _gtsam_find_args ${GTSAM_FIND_VERSION})
+  if(GTSAM_FIND_VERSION_EXACT)
+    list(APPEND _gtsam_find_args EXACT)
+  endif()
 endif()
 
-add_library(GTSAM::GTSAM INTERFACE IMPORTED GLOBAL)
-set_target_properties(GTSAM::GTSAM PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${GTSAM_INCLUDE_DIRS}"
-  INTERFACE_LINK_LIBRARIES "${GTSAM_LIBRARIES}")
+find_package(GTSAM CONFIG ${_gtsam_find_args})
+unset(_gtsam_find_args)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(GTSAM DEFAULT_MSG GTSAM_INCLUDE_DIRS GTSAM_LIBRARIES)
+if(GTSAM_FOUND AND TARGET gtsam)
+  # Promote to global visibility so the target is usable outside this
+  # directory scope.
+  set_target_properties(gtsam PROPERTIES IMPORTED_GLOBAL TRUE)
+
+  # GTSAM's config exports a bare 'gtsam' target. Create the namespaced
+  # GTSAM::GTSAM interface that gtsam_points and downstream consumers expect.
+  if(NOT TARGET GTSAM::GTSAM)
+    add_library(GTSAM::GTSAM INTERFACE IMPORTED GLOBAL)
+    set_target_properties(GTSAM::GTSAM PROPERTIES
+      INTERFACE_LINK_LIBRARIES gtsam
+    )
+  endif()
+
+  set(GTSAM_INCLUDE_DIRS ${GTSAM_INCLUDE_DIR})
+endif()
